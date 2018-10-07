a6697314c127b33e649c7a05c0b45fc7743dc121
spences10 <spences10apps@gmail.com>
2018-10-07 07:58:52 +0100

    use layout.css

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from './Header'
-import './index.css'
+import './layout.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
