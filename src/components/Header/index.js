096ec01211944c8aadcef9d8e121cc54a3c0feec
spences10 <spences10apps@gmail.com>
2018-10-07 07:48:51 +0100

    gatsby v2 changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,42 +1,43 @@
-import React from 'react';
-import Link from 'gatsby-link';
-const netlifyIdentity = require("netlify-identity-widget");
+import React from 'react'
+import { Link } from 'gatsby'
+const netlifyIdentity = require('netlify-identity-widget')
 
-export default class Header extends React.Component {  
-  componentDidMount(){    
-    netlifyIdentity.init();
+export default class Header extends React.Component {
+  componentDidMount() {
+    netlifyIdentity.init()
   }
-  
-  render(){
+
+  render() {
     return (
-    <div
-      style={{
-        background: 'rebeccapurple',
-        marginBottom: '1.45rem',
-      }}
-    >
       <div
         style={{
-          display: 'flex',
-          margin: '0 auto',
-          maxWidth: '960px',
-          padding: '1.45rem 1.0875rem',
-          justifyContent: 'space-between'
+          background: 'rebeccapurple',
+          marginBottom: '1.45rem'
         }}
       >
-        <h1 style={{ margin: 0 }}>
-          <Link
-            to="/"
-            style={{
-              color: 'white',
-              textDecoration: 'none',
-            }}
-          >
-            Gatsby
-          </Link>
-        </h1>
-        <div data-netlify-identity-menu></div>
+        <div
+          style={{
+            display: 'flex',
+            margin: '0 auto',
+            maxWidth: '960px',
+            padding: '1.45rem 1.0875rem',
+            justifyContent: 'space-between'
+          }}
+        >
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: 'white',
+                textDecoration: 'none'
+              }}
+            >
+              Gatsby
+            </Link>
+          </h1>
+          <div data-netlify-identity-menu />
+        </div>
       </div>
-    </div>)
+    )
   }
 }
